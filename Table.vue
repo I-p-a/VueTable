4a7f8157d9f2855f1de33c8ad1bf571b74c5c677
Petr73ru <tank73ru@gmail.com>
2017-11-21 22:19:19 +0400

    overflow fixed

--- Table.vue
+++ Table.vue
@@ -4,51 +4,60 @@
       v-bind:headers="headers"
       :items="items"
       hide-actions
-      class="elevation-1"
-    >
-      <template slot="items" slot-scope="props">
-        <td>
-          <v-btn fab dark small color="primary"
-            @click='removeItem(props.index)'>
-            <v-icon dark>remove</v-icon>
-          </v-btn>
-          <v-edit-dialog lazy>
-            <p style='min-width: 300px;'>{{ props.item.name }}</p>
-            <v-text-field
-                  slot="input"
-                  label="Edit"
-                  v-model="props.item.name"
-                  single-line
-                  counter
-                ></v-text-field>
-          </v-edit-dialog>
+      style='overflow:unset;'
+      class="elevation-1">
+      <template
+        slot="items"
+        slot-scope="props">
+        <tr>
+          <td>
+            <v-btn
+              fab
+              dark
+              small
+              color="primary"
+              style='float:left;'
+              @click='removeItem(props.index)'>
+              <v-icon dark>remove</v-icon>
+            </v-btn>
+            <v-edit-dialog lazy>
+              <p style='min-width: 20px;'>{{ props.item.name }}</p>
+              <v-text-field
+                slot="input"
+                label="Edit"
+                v-model="props.item.name"
+                single-line
+                counter></v-text-field>
+            </v-edit-dialog>
+          </td>
 
-        </td>
-        <td class="text-xs-right">
-          <v-edit-dialog lazy>
-            <p style='min-width: 300px;'>{{ props.item.count }}</p>
-            <v-text-field
-                  slot="input"
-                  label="Edit"
-                  v-model="props.item.count"
-                  single-line
-                  counter
-                ></v-text-field>
-          </v-edit-dialog>
-        </td>
-        <td class="text-xs-right">
-          <v-edit-dialog lazy>
-            <p style='min-width: 300px;'>{{ props.item.price }}</p>
-            <v-text-field
-                  slot="input"
-                  label="Edit"
-                  v-model="props.item.price"
-                  single-line
-                  counter
-                ></v-text-field>
-          </v-edit-dialog>
-        </td>
+          <td class="text-xs-right">
+            <v-edit-dialog lazy>
+              <p style='min-width: 20px;'>{{ props.item.count }}</p>
+              <v-text-field
+                slot="input"
+                label="Edit"
+                v-model="props.item.count"
+                single-line
+                counter></v-text-field>
+            </v-edit-dialog>
+          </td>
+
+          <td class="text-xs-right">
+            <v-edit-dialog lazy>
+              <p style='min-width: 20px;'>{{ props.item.price }}</p>
+              <v-text-field
+                slot="input"
+                label="Edit"
+                v-model="props.item.price"
+                single-line
+                counter></v-text-field>
+            </v-edit-dialog>
+          </td>
+        </tr>
+
       </template>
+
       <template slot="footer">
         <td colspan="100%">
           <strong>Итог: {{ Price }}</strong>
@@ -59,31 +68,31 @@
     <v-container
           fluid
           style="max-width: 400px; margin: auto;"
-          grid-list-lg
-    >
-        <v-card color="blue-grey darken-2" class="white--text">
+          grid-list-lg>
+        <v-card
+          color="blue-grey darken-2"
+          class="white--text">
           <v-card-title primary-title>
             <div class="headline">Unlimited music now</div>
             <div>
-
               <v-text-field
                 label="Name"
-                v-model='newItem.name'
-              ></v-text-field>
+                v-model='newItem.name'></v-text-field>
               <v-text-field
                 label="Count"
                 type="number"
-                v-model='newItem.count'
-              ></v-text-field>
+                v-model='newItem.count'></v-text-field>
               <v-text-field
                 label="Price"
                 type="number"
-                v-model='newItem.price'
-              ></v-text-field>
+                v-model='newItem.price'></v-text-field>
             </div>
           </v-card-title>
           <v-card-actions>
-            <v-btn flat dark @click='submit'>Submit</v-btn>
+            <v-btn
+              flat
+              dark
+              @click='submit'>Submit</v-btn>
           </v-card-actions>
         </v-card>
     </v-container>
